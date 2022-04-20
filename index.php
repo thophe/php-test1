503648168c95ed14554e2befd58cd404148dc791
tophe <cfieformation@gmail.com>
2022-04-20 16:52:43 +0200

    Phase 1 : Création du projet.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    $n = 1;    
+    $m = 2;
+    echo "instruction 1 : ".$n." ".$m."<br>";
+    $n++;
+    $m = $m + 2;
+    echo "instruction 2 : ".$n." ".$m."<br>";
+    $n++;
+    $m = $m + 2;
+    echo "instruction 3 : ".$n." ".$m."<br>";    
+    $n++;
+    $m = $m + 2;
+    echo "instruction 4 : ".$n." ".$m."<br>";
+
